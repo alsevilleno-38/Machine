484ac48f7d9c33c334f13a4f7b5af7e03b856048
aliengamer38 <aliengamer38@gmail.com>
2021-02-16 20:43:59 +0800

    news commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const str = "This is the time";
+
+const str_new = str.replace("This", "Note");
+console.log(str_new);
--- index.py
+++ index.py
@@ -1,35 +1,12 @@
-import os
-import csv
-import re
-
-link = "https://www.google.net"
-
-info = ["one", "two", "three", "four", "five", "six", "seven", "eight"]
-
-
 class Student:
-    def __init__(self, name, age, gender):
+    def __init__(self, name):
         self.name = name
-        self.age = age
-        self.gender = gender
-    def __repr__(self):
-        return f"({self.name}, {self.age}, {self.gender})"
 
-with open("img/future.jpeg", "rb") as img:
-    with open("img/copy.jpg", "wb") as copy:
-        for line in img:
-            copy.write(line)
-
-def decorator(callback):
-    def add(num1, num2):
-        return num1 + num2 + 10
-
-    return add
-
-@decorator
-def display():
-    print("This is a display function")
-
-print(display(1, 2))
-
-
+class PhD(Student):
+    def __init__(self, name, field):
+        super().__init__(name)
+        self.field = field
+    
+p1 = PhD("Doe", "Math")
+print(p1.name)
+print(p1.field)
