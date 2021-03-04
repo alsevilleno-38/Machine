08f212714a76e4f3d1f9f467ba0bb8af5863bc17
aliengamer38 <aliengamer38@gmail.com>
2021-03-04 09:25:25 +0800

    index

--- index.py
+++ index.py
@@ -1,17 +1,69 @@
-from tkinter import *
-from tkinter import messagebox
+import numpy as np
 
-root = Tk()
-e = Entry(root, width=50)
-e.insert(0, "Tkinter")
-e.grid(row=1, column=1)
-
-def screen():
-    messagebox.showinfo("Information", "Please close this")
-
-btn1 = Button(root, text="Click me", command=screen).grid(row=0, column=1)
-
-root.mainloop()
-
+def test():
+    try:
+        data = np.genfromtxt("data/data.txt", delimiter=",")
+        data = data.astype("int64")    
+        
+        # print(data[data > 0])    
+        # print(data.size)    
+        # print(np.any(data > 50))
+        print((data > 50) & (data < 100))
+    except OSError as os_error:
+        print("File does not exist")
 
 
+def test1():
+    a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    s = np.full(a.shape, True)
+    s[0, 0] = False
+    print(a[a > 1])
+
+
+def test2():
+    a = np.array(range(1, 31))
+    a = a.reshape(6, 5)
+    # print(a[[0, 4, 5]])
+    print(a)
+    print()
+    # print(a[[0, 4, 5], [0, 2, 3]])
+    # print()
+    # print(a[[0, 4, 5], 0:2])
+    print(a[[0, 1, 2, 3], [1, 2, 3, 4]])
+# test2()
+
+def test3():
+    j=np.arange(20,dtype=np.int)
+    site=np.ones((20,200),dtype=np.int)
+    sumkma=np.ones((100,20))
+    # print(site[j])
+    print(j)
+    # print(j.shape)
+    # print(np.newaxis)
+    # print(j[:,1].shape)
+
+    # [sumkma[site[x],x] for x in range(20)]
+
+    # sumkma[site[j],j]
+
+def test4():
+    # x = np.array([[ 0,  1,  2],
+    #           [ 3,  4,  5],
+    #           [ 6,  7,  8],
+    #           [ 9, 10, 11]])
+    a = np.array(range(1, 31))
+    a = a.reshape(6, 5)
+
+    print(a)
+    print()
+
+    rows = np.array([[0, 1],
+                 [0, 1]], dtype=np.intp)
+
+    columns = np.array([[2, 4],
+                    [2, 4]], dtype=np.intp)                    
+    print(a[])  
+
+    # print(rows)
+# test2()
+test4()
